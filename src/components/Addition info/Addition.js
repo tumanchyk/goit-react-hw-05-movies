f6901a719e095c6e21fa6b902867d3bac1c415fe
Tetiana Tsar <105817916+tumanchyk@users.noreply.github.com>
2023-03-27 01:50:37 +0300

    Update Addition.js

--- src/components/Addition info/Addition.js
+++ src/components/Addition info/Addition.js
@@ -1,6 +1,8 @@
 import { ViewSection } from "./Addition.styled";
 
 export const AdditionSection = () =>{
+  <>
     <ViewSection>Cast</ViewSection>
     <ViewSection>Reviews</ViewSection>
+  </>
 }
